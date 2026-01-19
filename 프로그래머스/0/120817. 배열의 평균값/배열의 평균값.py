c7fa9b2f2a2f34f673bd15e84025de99d118947e
Calcifer <155413606+kwonminseok242@users.noreply.github.com>
2026-01-19 17:11:32 +0900

    [level 0] Title: 배열의 평균값, Time: 0.01 ms, Memory: 8.98 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
@@ -0,0 +1,11 @@
+numbers = [2,3,4,5,6,7,8,9,10]
+
+def solution(numbers):
+    sum = 0
+    for i in range(len(numbers)):
+        sum += numbers[i]
+        
+    answer = sum/len(numbers)
+    return answer
+
+solution(numbers)
