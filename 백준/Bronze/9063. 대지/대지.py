fc731f30e3150d3389942f00d158d2a199984895
Calcifer <155413606+kwonminseok242@users.noreply.github.com>
2026-01-27 16:50:24 +0900

    [Bronze III] Title: 대지, Time: 1304 ms, Memory: 36352 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.py
@@ -0,0 +1,11 @@
+# 9063 대지
+n = int(input())
+x = []
+y = []
+
+for i in range(n):
+    a, b = map(int,input().split())
+    x.append(a)
+    y.append(b)
+
+print((max(x)-min(x))*((max(y))-(min(y))))
